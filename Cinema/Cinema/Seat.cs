3a2ef75718f3c72329f232a8c51954f5dc8cb7b5
Pascal <github@unrundead.eu>
2019-02-19 12:22:59 +0100

    Added Class Seat

---
+++ Cinema/Cinema/Seat.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Cinema
+{
+    class Seat : Button
+    {
+        public Seat() : base()
+        {
+            Background = Brushes.Green;
+            Margin = new System.Windows.Thickness(5);
+            Click += toggleColor;
+        }
+
+        public void toggleColor(object sender, RoutedEventArgs e)
+        {
+            if (Background == Brushes.Green)
+            {
+                Background = Brushes.Red;
+            }
+            else
+            {
+                Background = Brushes.Green;
+            }
+        }
+    }
+}
